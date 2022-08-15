c2bf51abbdda210e5ad8803768644eaab9a70b16
PeaceZay <zay.smith007@gmail.com>
2022-08-15 15:54:12 -0500

    Completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
+import React, { Component } from 'react';
 import './App.css';
+import PeopleCard from './PeopleCard';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      arrayOfPeople: []
+    }
+  };
+
+  componentDidMount(){
+    fetch("https://randomuser.me/api?results=25")
+    .then(res => res.json())
+    .then(data => {
+      console.log("data", data)
+      console.log("data res", data.results[0])
+      this.setState({
+        arrayOfPeople: data.results
+      })
+    })
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <ol>{this.state.arrayOfPeople.map((people, index) => {
+          console.log("person", people)
+          return (
+          <PeopleCard key={index}
+            gender={people["gender"]}
+            name={people.name.first}
+             city={people.location.city}
+             state={people.location.state}
+             country={people.location.country}
+            email={people.email}
+            // login={people.login}
+            // dob={people.login}
+            // registered={people.registered}
+            phone={people.phone}
+            cell={people.cell}
+            // id={people.id}
+            picture={people.picture.large}
+            ></PeopleCard>
+            )
+          })}</ol>
+          
+
+      </div>
+    )
+  }
 }
+  export default App
 
-export default App;
+  
+
+
+
+
+
+// APP should do this!!!
+// Spec 1: minimum 25 users
+// Spec 2: displays the user's name and thumbnail picture
+// Spec 3: When the "show details" button is clicked the user's 
+// details are revealed and button text turns to "hide details"
+// Spec 4 When "hide details" is clicked the user's details disappear.
